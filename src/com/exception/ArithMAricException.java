f479030390202d7e4d1d60a5276b1e848f89fb44
akashvelmurugan <akashvelmurugan125@gmail.com>
2022-02-10 11:14:34 +0530

    TYpes Of RunTime Exception

---
+++ src/com/exception/ArithMAricException.java
@@ -0,0 +1,12 @@
+package com.exception;
+
+public class ArithMAricException {
+public static void main(String[] args) {
+	
+
+int a=10;
+int b=0;
+int c=a/b;
+System.out.println(c);	
+}	
+}
---
+++ src/com/exception/NullPointerException.java
@@ -0,0 +1,16 @@
+package com.exception;
+
+
+     //StringIndexOutOfBoundsException
+	//NullPointerException
+
+public class NullPointerException {
+	public static void main(String[] args) {
+
+		String string1=""; 
+
+		String string2="JAVA";
+
+		System.out.println(string1.charAt(0));
+	}
+}
